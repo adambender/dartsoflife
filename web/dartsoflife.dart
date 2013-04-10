26754786460dcac278a804e141da71ca884ea4e6
adambender <adambender@gmail.com>
2013-04-10 10:52:23 -0700

    cleaned up first renderer, still needs to acknowledge edges as dead cells.

--- web/dartsoflife.dart
+++ web/dartsoflife.dart
@@ -1,15 +1,49 @@
 import 'dart:html';
 import 'package:web_ui/web_ui.dart';
-
+import 'package:logging/logging.dart';
 
 /**
  * Learn about the Web UI package by visiting
  * http://www.dartlang.org/articles/dart-web-components/.
  */
-void main() {
-  // Enable this to use Shadow DOM in the browser.
+
+final List<List<int>> model = [[0,0,1,0,0,0,0,0,0,0,0],
+                               [1,0,1,0,0,0,0,0,0,0,0],
+                               [0,1,1,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0],
+                               [0,0,0,0,0,0,0,0,0,0,0]];
+void main() {  // Enable this to use Shadow DOM in the browser.
   //useShadowDom = true;
+  _setupLogger();
 }
+
+void importModel(){
+  document.query('#gameboard').xtag.setExternalModel(model);  
+}
+
 void nextStep(){
   document.query('#gameboard').xtag.nextStep();    
 }
+void _setupLogger() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord r) {
+    StringBuffer sb = new StringBuffer();
+    sb
+    ..write(r.time.toString())
+    ..write(":")
+    ..write(r.loggerName)
+    ..write(":")
+    ..write(r.level.name)
+    ..write(":")
+    ..write(r.sequenceNumber)
+    ..write(": ")
+    ..write(r.message.toString());
+    print(sb.toString());
+  });
+}
+
--- web/gameboard.dart
+++ web/gameboard.dart
@@ -3,64 +3,92 @@ library gol;
 import 'package:web_ui/web_ui.dart';
 import 'dart:html';
 import 'dart:async';
+import 'package:logging/logging.dart';
 
 class GameBoard extends WebComponent {
-  final List<List<Cell>>model = new List<List<Cell>>();
+  static final Logger logger = new Logger("GameBoard");
+  final List<List<Cell>> model = new List<List<Cell>>();
   int _height = 0;
   int _width = 0;
   
   GameBoard(){
-   _resetBoard();
+    
   }
   
+  void setExternalModel(List<List<int>> externalModel){
+    model.clear();
+    for(var i = 0; i < externalModel.length; i++){
+      List<Cell> col = new List<Cell>();
+      for(var j = 0; j< externalModel[i].length; j++){
+        col.add(new Cell(new Point(j,i), externalModel[i][j] == 1));
+      }
+      height = externalModel.length;
+      width = externalModel[0].length;
+      model.add(col);
+    }
+  }
   //Any live cell with fewer than two live neighbours dies, as if caused by under-population.
   //Any live cell with two or three live neighbours lives on to the next generation.
   //Any live cell with more than three live neighbours dies, as if by overcrowding.
   //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
   void nextStep(){
     //process rules 
-    for(int i = 0; i < _height; i++){
-      for(int j = 0; j < _width; j++){
-        int neighborCount = _getAliveNeighborCount(i,j);
-        bool isAlive = model[j][i].alive;
-        model[j][i].alive = (isAlive && neighborCount < 2) ||  (isAlive && neighborCount == 2) ||  neighborCount == 3;
+    var nextModel = new List<List<Cell>>();
+    for(var i = 0; i < _height; i++){
+      List<Cell> col = new List<Cell>();
+      logger.fine("xxxxxxxxxxxxxxxxxxxxxxxxx");
+      for(var j = 0; j < _width; j++){
+        var neighborCount = _getAliveNeighborCount(i,j);
+        var isAlive = model[i][j].alive;
+        var stillAlive = (isAlive && neighborCount == 2) || neighborCount == 3;
+        var nextCell = stillAlive ? new Cell.living(model[i][j].coord): new Cell.dead(model[i][j].coord);
+        col.add(nextCell);
+        logger.fine(nextCell.toString());
+      }
+      nextModel.add(col);
+      logger.fine("-------------------------");
+    }
+    for(var i = 0; i < _height; i++){
+      for(var j = 0; j < _width; j++){
+        model[i][j] = nextModel[i][j];
       }
     }
   }
   
-  void _getAliveNeighborCount(int i, int j){
-    int count = 0;
-    if(0 < i && model[j][i-1]){//top
+  int _getAliveNeighborCount(int i, int j){
+    var count = 0;
+    if(0 < i && model[i-1][j].alive){//top
       count++;
     } 
-    if(i < _width - 1 && model[j][i+1]){//bottom
+    if(i < _height - 1 && model[i+1][j].alive){//bottom
       count++;
     }
-    if(0 < j && model[j-1][i]){//left
+    if(0 < j && model[i][j-1].alive){//left
       count++;
     }
-    if(j < _height - 1 && model[j+1][i]){//right
+    if(j < _width - 1 && model[i][j+1].alive){//right
       count++;
     }
-    if(0 < i && 0 < j && model[j-1][i-1]){//top left
+    if(0 < i && 0 < j && model[i-1][j-1].alive){//top left
       count++;
     }
-    if(i < _width && j < _height && model[j+1][i+1]){//bottom right
+    if(i < _height - 1 && j < _width - 1 && model[i+1][j+1].alive){//bottom right
       count++;
     }
-    if(0 < i && j < _height && model[j+1][i-1]){//top right
+    if(0 < i && j < _width -1 && model[i-1][j+1].alive){//top right
       count++;
     }
-    if(i < _width && 0 < j && model[j-1][i+1]){//bottom left
+    if(i < _height -1 && 0 < j && model[i+1][j-1].alive){//bottom left
       count++;
     }
     return count;
   }
   
   void _resetBoard(){
-    for(int i = 0; i < _height; i++){
+    model.clear();
+    for(var i = 0; i < _height; i++){
       List<Cell> col = new List<Cell>();
-      for(int j = 0; j < _width; j++){
+      for(var j = 0; j < _width; j++){
         col.add(new Cell(new Point(j,i), j % 3 == 0));
       }
       model.add(col);
@@ -70,18 +98,19 @@ class GameBoard extends WebComponent {
   int get height => _height;
   void set height(int value) {
     _height = value;
-    _resetBoard();
   }
   
   int get width => _width;
   void set width(int value) {
     _width = value;
-    _resetBoard();
   }
 }
 
 class Cell{
   final bool alive;
   final Point coord;
-  Cell(Point this.coord, bool this.alive){}
+  Cell(this.coord, this.alive);
+  Cell.living(Point coord) : this.coord = coord, alive = true;
+  Cell.dead(Point coord) : this.coord = coord, alive = false;
+  String toString() => '"x":$coord.x, "y":$coord.y, "alive":$alive';
 }
